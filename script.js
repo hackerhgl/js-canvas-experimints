78869f566420458ccd991a34e873a5af3e767b6b
hackerhgl <hackerhgl@gmail.com>
2021-08-18 02:02:50 +0500

    Now we can copy & paste chunk of canvas & animate it

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-const FONT_SIZE = 120;
+const FONT_SIZE = 200;
+const d = document.getElementById("debug");
+
+function c(...args) {
+    d.innerHTML=args.join(', ')
+}
+
+var animation
 
 (function name() {
     const canvas = document.getElementById("canvas");
@@ -7,10 +14,52 @@ const FONT_SIZE = 120;
     ctx = canvas.getContext('2d');
     const h = canvas.height;
     const w = canvas.width;
+    const ww = w/2;
+    const hh = h/2;
 
-    ctx.font = FONT_SIZE+"px Arial";
-    const name = "Hamza Iqbal";
-    // ctx.translate(w/2, h/2);
-    ctx.fillText(name, 0, FONT_SIZE);
+    ctx.font = `900 ${FONT_SIZE}px Arial`;
+    const string = "Hamza Iqbal";
+    ctx.textBaseline = 'middle'; 
+    ctx.textAlign = 'center';
+
+    ctx.fillText(string, 0, 0);
+    // ctx.fillText(string, ww, hh);
+
+    const o = 100;
+    var image = ctx.getImageData(0, 0, o, o);
+    var pixels = image.data;
+
+    console.log(image);
+
+    // ctx.putImageData({ ... }, w-100, h-100);
+    const oo = 200;
+    count = 0;
+    countY = 0;
+
+    function loop() {
+        console.log('request', count);
+        const color = [];
+        for (let j=0; j<4; j++) {
+            color.push(pixels[count+j]);
+        }
+        p = count/4;
+        y = Math.floor(p/o);
+        x =   p- (y*o);
+        console.log();
+        const imageData = new ImageData(Uint8ClampedArray.from(color), 1,1);
+        ctx.putImageData(imageData, oo+x,oo+y)
+        if (count >= pixels.length) {
+            console.log('count', stop);
+            cancelAnimationFrame(animation)
+        }
+        count += 4;
+        animation = requestAnimationFrame(() => loop());
+    }
+
+    loop()
+
+    
+
+
 
 })();
