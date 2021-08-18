226223ddccf96ca553ea4eeb7397bc74bdb6c171
hackerhgl <hackerhgl@gmail.com>
2021-08-18 17:42:05 +0500

    park

--- learning/script.ts
+++ learning/script.ts
@@ -1,28 +1,32 @@
 import { getPixelColor, isEmptyPixel } from "../pixels";
 import { drawText, initCanvas } from "../canvas";
 
+const PIXEL_SIZE = 4;
+
 
 (function name() {
     const { ctx, h, w } = initCanvas();
     const string = "Hamza Iqbal";
     drawText(ctx, string);
 
-    const o = 100;
+    const offset = {x: 20, y: 100};
 
-    var image = ctx.getImageData(0, 0, o, o);
+    var image = ctx.getImageData(0, 0, offset.x, offset.y);
+    console.log(image);
+    
     var pixels = image.data;
 
-    const limit = o*o*4; 
+    const limit = offset.x*offset.y*PIXEL_SIZE; 
     const oo = 200;
     let count = 0;
+    const chunks = {x:1, y: 1}; 
     
     function loop() {
         const color = getPixelColor(pixels, count);
-        console.log('trigger', count, limit, color);
         const p = count/4;
-        const y = Math.floor(p/o);
-        const x = p - (y*o);
-        const imageData = new ImageData(Uint8ClampedArray.from(color), 1,1);
+        const y = Math.floor(p/offset.x);
+        const x = p - (y*offset.x);
+        const imageData = new ImageData(Uint8ClampedArray.from(color), 1, 1);
         ctx.putImageData(imageData, oo+x,oo+y)
         if (count >= limit) {
             return;
@@ -35,7 +39,7 @@ import { drawText, initCanvas } from "../canvas";
                 break;
             }
         }
-        requestAnimationFrame(loop);
+        // requestAnimationFrame(loop);
     }
 
     loop();
